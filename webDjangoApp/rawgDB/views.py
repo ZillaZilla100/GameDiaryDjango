c340ef846a3d849383326dcde34ecae62ca37fc5
Rod Owens <r.m.owens100@gmail.com>
2023-04-22 07:50:50 -0500

    1st yeet yeet

---
+++ webDjangoApp/rawgDB/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from manage import x
+
+# Create your views here.
+
+def cracker(request):
+    return HttpResponse('yeet yeet yeet')
+
+
+
